388ac835b1d3107359f01837af8b7d9a6e9d970b
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-21 00:23:03 +0000

    navy

--- navy_schema.sql
+++ navy_schema.sql
@@ -1,1 +1,31 @@
-SELECT 'hello, PG';
+CREATE TABLE fleet (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE ship (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(25) NOT NULL,
+  date_built DATE,
+  fleet_id INTEGER REFERENCES fleet(id)
+);
+
+CREATE TABLE duty (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255)
+);
+
+CREATE TABLE sailor (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  birth_date DATE
+);
+
+CREATE TABLE log (
+  day DATE NOT NULL,
+  sailor_id INTEGER REFERENCES sailor(id) NOT NULL,
+  duty_id INTEGER REFERENCES duty(id),
+  ship_id INTEGER REFERENCES ship(id),
+  rank VARCHAR(25),
+  PRIMARY KEY (day,sailor_id)
+);
